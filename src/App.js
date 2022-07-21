011b8578d86c3f074095ab4d4c0b916d548df479
Christine Taylor <ctay47@icloud.com>
2022-07-21 13:45:45 -0400

    completed with Partner Adnan

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import NavBar from './NavBar';
+import Home from './Home';
+import LogEdit from './LogEdit';
+import Logs from './Logs';
+import LogNew from './LogNew';
+import LogDetail from './LogDetail';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<Logs />} />
+            <Route path="/logs/new" element={<LogNew />} />
+            <Route path="/logs/:index" element={<LogDetail />} />
+            <Route path="/logs/:index/edit" element={<LogEdit />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
